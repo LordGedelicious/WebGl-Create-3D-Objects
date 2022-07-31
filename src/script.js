7d627633d2cd408ba3fbeb15ae4d600e918ee41e
Gede Prasidha <gede.prasidha@gmail.com>
2022-07-31 21:11:08 +0700

    final update
    gabisa buat bangun keduanya :""

--- src/script.js
+++ src/script.js
@@ -66,10 +66,10 @@ let vertCode = 'attribute vec3 position;' +
     'uniform mat4 Pmatrix;' +
     'uniform mat4 Vmatrix;' +
     'uniform mat4 Mmatrix;' +
-    'attribute vec3 color;' + //the color of the point
+    'attribute vec3 color;' +
     'varying vec3 vColor;' +
 
-    'void main(void) { ' + //pre-built function
+    'void main(void) { ' + 
     'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);' +
     'vColor = color;' +
     '}';
